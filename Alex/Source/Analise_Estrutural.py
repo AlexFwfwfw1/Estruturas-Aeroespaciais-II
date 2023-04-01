51c0e1cb11d1193ad0c90eab728fee1951b07e92
AlexFwfwfw1 <alexandreoliveira2001g@gmail.com>
2023-04-02 00:27:57 +0100

    TEnsoes Diretas Feitas!!

--- Alex/Source/Analise_Estrutural.py
+++ Alex/Source/Analise_Estrutural.py
@@ -4,47 +4,63 @@ import Definicao_Laminado
 import Carregamento
 
 import math
+import numpy as np
 
-PARTICOES_CADA_TROCO = 100
-
-def Obter_Geometria_Media(z, Laminado_1,Laminado_2):
-    
-    Altura = 0.9*(1 - 0.5*z/COMPRIMENTO_FUSELAGEM)
-    Diametro = 1.5*(1-0.7*z/COMPRIMENTO_FUSELAGEM)
-    
-    Altura_Media = Altura - Laminado_2.Espessura/2 
-    Raio_Medio = Diametro/2 - Laminado_1.Espessura/2
-    
-    return Altura_Media, Raio_Medio
+NUMERO_DE_PONTOS = 100
 
 # TEM DE SER UM FUNCAO AO CENTROIDE
-def Obter_Coordenadas_Vertical_Esquerda(S_Coordenada, z, Altura_Media, Raio_Medio):
-    Coordenada_X = -Raio_Medio
-    Coordenada_Y = - Altura_Media + S_Coordenada
-    return Coordenada_X,Coordenada_Y
-
-def Obter_Coordenadas_Semicirculo(S_Coordenada, z, Altura_Media, Raio_Medio):
-    Coordenada_X = -Raio_Medio*math.cos(S_Coordenada)
-    Coordenada_Y = -Raio_Medio*math.sin(S_Coordenada) 
-    return Coordenada_X,Coordenada_Y
-    
 
 def Analise_Tensao_Direta(z, Laminado_1, Laminado_2, Laminado_3):
-    
+
     Altura_Media, Raio_Medio = Propriadades.Obter_Geometria_Media(z)
-    Centroide_Y, Centroide_X = Propriadades.Obter_Centroide(z)
-   
+    Centroide_X, Centroide_Y = Propriadades.Obter_Centroide(z)
+    I_xx, I_yy = Propriadades.Obter_Segundo_Momentos_De_Area(z)
+
     Propriadades_da_Seccao = Propriadades.Obter_Propriades(z)
-    
-    Barra_Vertical_D = Propriadades_da_Seccao.Barra_Vertical_D
-    Barra_Vertical_E = Propriadades_da_Seccao.Barra_Vertical_E
+
+    Barra_Vertical = Propriadades_da_Seccao.Barra_Vertical_D
     Barra_Horizontal = Propriadades_da_Seccao.Barra_Horizontal
-    SemiCirculo      = Propriadades_da_Seccao.SemiCirculo
-    Tensores         = Propriadades_da_Seccao.Tensores
+    SemiCirculo = Propriadades_da_Seccao.SemiCirculo
+    Tensores = Propriadades_da_Seccao.Tensores
+
+    Forca_X, Forca_Y = Carregamento.Obter_Forcas(z)
+    Momento_X, Momento_Y = Carregamento.Obter_Momentos(z)
+
+    # Analise comeca no meio da barra horizontal.
+
+    Constante_Em_X = (Momento_Y / I_yy)
+    Constante_Em_Y = (Momento_X / I_xx)
+
+    Tensoes_Diretas = []
     
-    Troços = {Barra_Vertical_D, Barra_Vertical_E, Barra_Horizontal, SemiCirculo, Tensores}
-    
-    Forca_Vertical, Forca_Horizontal = Carregamento.Obter_Forcas(z)
-    Momento_Vertical, Momento_Horizontal = Carregamento.Obter_Momentos(z)
-    
+    #VIGA HORIZONTAL, MEIO ATE PONTA
+    for Ponto_S in np.linspace(0, Raio_Medio , NUMERO_DE_PONTOS):
+        Coordenada_X = Ponto_S - Centroide_X
+        Coordenada_Y = -Altura_Media - Centroide_Y
+        Tensao_Direta = Barra_Horizontal.Ex * (Constante_Em_X*Coordenada_X + Constante_Em_Y*Coordenada_Y)
+        Tensoes_Diretas.append({"Coords" : (Coordenada_X, Coordenada_Y), "Tensao" : Tensao_Direta})
         
+    for Ponto_S in np.linspace(0, Altura_Media , NUMERO_DE_PONTOS):
+        Coordenada_X = -Raio_Medio - Centroide_X
+        Coordenada_Y = - Altura_Media + Ponto_S - Centroide_Y
+        Tensao_Direta = Barra_Vertical.Ex * (Constante_Em_X*Coordenada_X + Constante_Em_Y*Coordenada_Y)
+        Tensoes_Diretas.append({"Coords" : (Coordenada_X, Coordenada_Y), "Tensao" : Tensao_Direta})
+        
+    for Ponto_S in np.linspace(0, math.pi , NUMERO_DE_PONTOS):
+        Coordenada_X = -Raio_Medio*math.cos(Ponto_S) - Centroide_X
+        Coordenada_Y = -Raio_Medio*math.sin(Ponto_S) - Centroide_Y
+        Tensao_Direta = SemiCirculo.Ex * (Constante_Em_X*Coordenada_X + Constante_Em_Y*Coordenada_Y)
+        Tensoes_Diretas.append({"Coords" : (Coordenada_X, Coordenada_Y), "Tensao" : Tensao_Direta})
+        
+    for Ponto_S in np.linspace(0, Altura_Media , NUMERO_DE_PONTOS):
+        Coordenada_X = Raio_Medio - Centroide_X
+        Coordenada_Y = - Ponto_S - Centroide_Y
+        Tensao_Direta = Barra_Vertical.Ex * (Constante_Em_X*Coordenada_X + Constante_Em_Y*Coordenada_Y)
+        Tensoes_Diretas.append({"Coords" : (Coordenada_X, Coordenada_Y), "Tensao" : Tensao_Direta})
+        
+    for Ponto_S in np.linspace(0, Raio_Medio , NUMERO_DE_PONTOS):
+        Coordenada_X = Ponto_S - Raio_Medio - Centroide_X
+        Coordenada_Y = - Altura_Media - Centroide_Y
+        Tensao_Direta = Barra_Horizontal.Ex * (Constante_Em_X*Coordenada_X + Constante_Em_Y*Coordenada_Y)
+        Tensoes_Diretas.append({"Coords" : (Coordenada_X, Coordenada_Y), "Tensao" : Tensao_Direta})
+
