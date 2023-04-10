e5c1e93200c61f7068c37330fb051103ab5eea29
Manuel Alves <manuellandeiro@sapo.pt>
2023-04-10 10:35:34 +0000

    Correção área encerrada pela secção

--- Source Code/qs_0.py
+++ Source Code/qs_0.py
@@ -32,7 +32,7 @@ def qs_0(z, Laminado_1, Laminado_2, Laminado_3, b):
     y_1, y_2, y_3, y_4 = Coordenadas_y
     w_linha = Geometria_Media[1]/2
     h_linha = Geometria_Media[0]
-    A_total_i = Geometria_Media[2]
+    A_total_i = h_linha * w_linha + math.pi * w_linha**2
 
     I_C3 = A_i/2 *  y_2 * w_linha**2 - B_i /6* w_linha**3
     I_34 = A_i * (Espessura_laminado1 * y_3 * h_linha**2 /2 + Espessura_laminado1 * h_linha**3 /6 + Area_laminado3 * y_3 * h_linha) + B_i * (Espessura_laminado1 * x_3 * h_linha**2 /2 + Area_laminado3 * x_3 * h_linha) + q3 * h_linha 
