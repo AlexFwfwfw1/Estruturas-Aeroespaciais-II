0fbbab1e223f1e03ff827b92a7eb74b32189bfe7
Manuel Alves <manuellandeiro@sapo.pt>
2023-04-20 22:55:20 +0100

    n/a

--- Source Code/Analise_Estrutural.py
+++ Source Code/Analise_Estrutural.py
@@ -9,7 +9,7 @@ NUMERO_DE_PONTOS = 15
 # TEM DE SER UM FUNCAO AO CENTROIDE
 
 def Analise_Total(Geometria_Media, Centroide, Segundo_Momentos_De_Area, Forcas_Afilamento, Momentos, Elasticidades, Espessuras, Coordenadas_Y, Coordenadas_X):
-    Altura_Media, Diametro_Medio, Area_Total, Area_Varrida = Geometria_Media
+    Altura_Media, Diametro_Medio, Area_Total = Geometria_Media
     Centroide_X, Centroide_Y = Centroide
     I_xx, I_yy, Ixy = Segundo_Momentos_De_Area
     Ex_1,Ex_2,Ex_3 = Elasticidades
@@ -31,34 +31,32 @@ def Analise_Total(Geometria_Media, Centroide, Segundo_Momentos_De_Area, Forcas_A
 
     Qb3 = Ex_2 * (Constante_A_Corte*Raio_Medio*Y2*T2 + Constante_B_Corte*T2*(-0.5*Raio_Medio**2))
     Qb4 = Ex_1 * (Constante_A_Corte*(Altura_Media*Y3*T1 + T1*0.5*Altura_Media**2 + A3*Y3) + Constante_B_Corte*(T1*X3*Altura_Media+A3*X3)) + Qb3
-    Qb1 = Ex_1 * (Constante_A_Corte*(T1*Raio_Medio*Y1*math.pi + T1*(Raio_Medio**2)*(2) + A3*Y4) + Constante_B_Corte*(A3*X4)) + Qb4
+    Qb1 = Ex_1 * (Constante_A_Corte*(T1*Raio_Medio*Y1*math.pi + T1*(Raio_Medio**2)*(1-math.cos(math.pi)) + A3*Y4) + Constante_B_Corte*(-T1*(Raio_Medio**2)*math.sin(math.pi)+A3*X4)) + Qb4
     Qb2 = Ex_1 * (Constante_A_Corte*(T1*(Y1*Altura_Media - 0.5*Altura_Media**2) + Y1*A3) + Constante_B_Corte*(T1*X1*Altura_Media + A3*X1)) + Qb1
     QbC = Ex_2 * (Constante_A_Corte*(T2*Y2*Raio_Medio + A3*Y2) + Constante_B_Corte*(T2*(X2*Raio_Medio - 0.5*Raio_Medio**2) + A3*X2)) + Qb2
     
-    #Nao esta a dar zero pela simples razao de que trocos que se costumavam se anular, nao se estao a anular devido a trocos com modulos elasticos diferentes.
-    #Causando corte nao nulo no ponto final. Possiveis solucoes sao colocar as contribuicoes no troco seguinte mas nao sei como implementar isto matematicamente.
-    
     # #valores a retirar do filipe
-    Px_1, Px_2, Px_3, Px_4 = 12, 12, 12, 12
-    Py_1, Py_2, Py_3, Py_4 = 12, 12, 12, 12
-    Sx = 76
+    # Px_1, Px_2, Px_3, Px_4 = 12, 12, 12, 12
+    # Py_1, Py_2, Py_3, Py_4 = 12, 12, 12, 12
+    # Sx = 76
     
+    # A_Varrida = Altura_Media * Raio_Medio + math.pi/2* Raio_Medio**2
 
-    I_C3 = Constante_A_Corte/2 *  Y2 * Raio_Medio**2 - Constante_B_Corte /6* Raio_Medio**3
-    I_34 = Constante_A_Corte * (T1 * Y3 * Altura_Media**2 /2 + T1 * Altura_Media**3 /6 + A3 * Y3 * Altura_Media) + Constante_B_Corte * (T1 * X3 * Altura_Media**2 /2 + A3 * X3 * Altura_Media) + Qb3 * Altura_Media 
-    I_41 = Constante_A_Corte * (T1 * Raio_Medio**2 * Y1 * math.pi**2 /2 + T1 * Raio_Medio**3 * math.pi + A3 * Y4 * Raio_Medio * math.pi) + Constante_B_Corte * (-2* T1 * Raio_Medio**3 + A3 * X4 * Raio_Medio * math.pi) + Qb4 * math.pi * Raio_Medio
-    I_12 = Constante_A_Corte * (T1 * Y1 * Altura_Media**2 /2 - T1 * Altura_Media**3 /6 + Y1 * A3 * Altura_Media) + Constante_B_Corte * (T1 * X1 * Altura_Media**2 /2 + Constante_B_Corte * X1 * Altura_Media) + Qb1 * Altura_Media
-    I_2C = Constante_A_Corte * (T2 * Y2 * Raio_Medio**2 /2 + A3 * Y2 * Raio_Medio) + Constante_B_Corte * (T2 * X2 * Raio_Medio**2 /2 - T2 * Raio_Medio**3 /6 + A3 * X2 * Raio_Medio) + Qb2 * Raio_Medio
+    # I_C3 = Constante_A_Corte/2 *  Y2 * Raio_Medio**2 - Constante_B_Corte /6* Raio_Medio**3
+    # I_34 = Constante_A_Corte * (T1 * Y3 * Altura_Media**2 /2 + T1 * Altura_Media**3 /6 + A3 * Y3 * Altura_Media) + Constante_B_Corte * (T1 * X3 * Altura_Media**2 /2 + A3 * X3 * Altura_Media) + Qb3 * Altura_Media 
+    # I_41 = Constante_A_Corte * (T1 * Raio_Medio**2 * Y1 * math.pi**2 /2 + T1 * Raio_Medio**3 * math.pi + A3 * Y4 * Raio_Medio * math.pi) + Constante_B_Corte * (-2* T1 * Raio_Medio**3 + A3 * X4 * Raio_Medio * math.pi) + Qb4 * math.pi * Raio_Medio
+    # I_12 = Constante_A_Corte * (T1 * Y1 * Altura_Media**2 /2 - T1 * Altura_Media**3 /6 + Y1 * A3 * Altura_Media) + Constante_B_Corte * (T1 * X1 * Altura_Media**2 /2 + Constante_B_Corte * X1 * Altura_Media) + Qb1 * Altura_Media
+    # I_2C = Constante_A_Corte * (T2 * Y2 * Raio_Medio**2 /2 + A3 * Y2 * Raio_Medio) + Constante_B_Corte * (T2 * X2 * Raio_Medio**2 /2 - T2 * Raio_Medio**3 /6 + A3 * X2 * Raio_Medio) + Qb2 * Raio_Medio
 
     # #integração sentido horário
-    I_total = - Altura_Media * I_C3 - Raio_Medio * I_34 - Raio_Medio * I_41 - Raio_Medio * I_12 - Altura_Media * I_2C
+    # I_total = - Altura_Media * I_C3 - Raio_Medio * I_34 - Raio_Medio * I_41 - Raio_Medio * I_12 - Altura_Media * I_2C
 
-    PxBraço_1, PxBraço_2, PxBraço_3, PxBraço_4 = Px_1 * 0, Px_2 * Altura_Media , Px_3 * Altura_Media  , Px_4 * 0
-    PyBraço_1, PyBraço_2, PyBraço_3, PyBraço_4 = Py_1 * Raio_Medio, Py_2 * Raio_Medio , Py_3 * (- Raio_Medio)  , Py_4 * (- Raio_Medio)
-    Soma_PxB = PxBraço_1 + PxBraço_2 + PxBraço_3 + PxBraço_4
-    Soma_PyB = PyBraço_1 + PyBraço_2 + PyBraço_3 + PyBraço_4
+    # PxBraço_1, PxBraço_2, PxBraço_3, PxBraço_4 = Px_1 * 0, Px_2 * Altura_Media , Px_3 * Altura_Media  , Px_4 * 0
+    # PyBraço_1, PyBraço_2, PyBraço_3, PyBraço_4 = Py_1 * Raio_Medio, Py_2 * Raio_Medio , Py_3 * (- Raio_Medio)  , Py_4 * (- Raio_Medio)
+    # Soma_PxB = PxBraço_1 + PxBraço_2 + PxBraço_3 + PxBraço_4
+    # Soma_PyB = PyBraço_1 + PyBraço_2 + PyBraço_3 + PyBraço_4
 
-    qs_0_i = (Sx * 1.2 - I_total + Soma_PxB - Soma_PyB) / (2 * A_Varrida) #Area Errada, esta area nao é a area da seccao mas é a area interior á seccao media. Nao alterei manel. Altera Tu.
+    # qs_0_i = (Sx * 1.2 - I_total + Soma_PxB - Soma_PyB) / (2 * A_Varrida) #Area Errada, esta area nao é a area da seccao mas é a area interior á seccao media. Nao alterei manel. Altera Tu.
 
     
     Qb1, Qb2, Qb3, Qb4, QbC = 0,0,0,0,0
@@ -72,9 +70,10 @@ def Analise_Total(Geometria_Media, Centroide, Segundo_Momentos_De_Area, Forcas_A
         
         Tensao_de_Corte = Fluxo_Corte/T2
         
-        # FS_Falha = Teste_de_falha.Falha(Tensao_Direta, 0, Tensao_de_Corte)
+        # FS_Falha = (Tensao_Direta, 0, Tensao_de_Corte)
         # if FS_Falha > 1:
-        #     return FS_Falha
+            # return FS_Falha
+        
         
     Qb3 = Fluxo_Corte
 
